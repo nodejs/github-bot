a18378f2ec7293e377c9ca9051603b7ba9bcd73d
Phillip Johnsen <johphi@gmail.com>
2016-02-26 09:22:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node.js GitHub Bot
+
+What's this?? ATM an experiment to see if a bot could be helpful for the Node.js project, to automate all the things!
+
+## Contributing
+
+Please do, contributions are more than welcome!
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const pollTravis = require('./lib/pollTravis')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.all('/hooks/github', (req, res) => {
+  if (wasPullRequestOpened(req)) {
+    const repo = req.body.repository
+
+    console.log(`* ${repo.owner.login}/${repo.name}/#${req.body.number} Opened, starting build checks!`)
+    pollTravis.pollAndComment(repo.owner.login, repo.name, parseInt(req.body.number))
+  }
+
+  res.end()
+})
+
+// to trigger polling manually
+app.get('/pr/:owner/:repo/:prId', (req, res) => {
+  pollTravis.pollAndComment(req.params.owner, req.params.repo, parseInt(req.params.prId))
+  res.end()
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port', port)
+})
+
+function wasPullRequestOpened (req) {
+  const githubEvent = req.headers['x-github-event'] || ''
+  const githubAction = req.body.action || ''
+  return githubEvent === 'pull_request' && githubAction === 'opened'
+}
