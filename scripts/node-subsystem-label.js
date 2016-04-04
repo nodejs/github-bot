2b099dba2b9c20c626b31f4c0ead2f67a71a5ca5
Phillip Johnsen <johphi@gmail.com>
2016-04-04 22:18:48 +0200

    Auto labeling PRs with subsystem labels.
    Started out with two labels in this iteration: c++ and test.

---
+++ scripts/node-subsystem-label.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const debug = require('debug')('node_subsystem_label')
+
+const nodeRepo = require('../lib/node-repo')
+
+module.exports = function (app) {
+  app.on('pull_request.opened', handlePrCreated)
+
+  function handlePrCreated (event, owner, repo) {
+    const prId = event.number
+    // subsystem labelling is for node core only
+    if (repo !== 'node') return
+
+    debug(`/${owner}/${repo}/pull/${prId} opened`)
+    // by not hard coding the owner repo to nodejs/node here,
+    // we can test these this script in a different repo than
+    // *actual* node core as long as the repo is named "node"
+    nodeRepo.resolveLabelsThenUpdatePr({ owner, repo, prId })
+  }
+}
