da57626aec1a06019a30c8ba3906a91c825e296c
William <william.kapke@gmail.com>
2016-04-08 16:48:46 +0000

    allow travis script to run on citgm and readable-stream

--- scripts/display-travis-status.js
+++ scripts/display-travis-status.js
@@ -2,12 +2,13 @@
 
 const debug = require('debug')('display_travis_status')
 const pollTravis = require('../lib/pollTravis')
+const enabledRepos = ['citgm', 'readable-stream', 'nodejs.org']
 
 module.exports = function (app) {
   app.on('pull_request.opened', (event) => {
     const owner = event.repository.owner.login
     const repo = event.repository.name
-    if (repo !== 'nodejs.org') return
+    if (!~enabledRepos.indexOf(repo)) return
 
     debug(`/${owner}/${repo}/pull/${event.number} opened`)
     pollTravis.pollThenComment(owner, repo, event.number)
@@ -18,7 +19,7 @@ module.exports = function (app) {
     const owner = req.params.owner
     const repo = req.params.repo
     const id = req.params.id
-    if (repo === 'nodejs.org') {
+    if (~enabledRepos.indexOf(repo)) {
       pollTravis.pollThenComment(owner, repo, parseInt(id, 10))
     }
     res.end()
