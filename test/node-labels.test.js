d719ee1f18680e7850d5e8b7136eea72f1221150
Phillip Johnsen <johphi@gmail.com>
2016-04-27 20:57:07 +0200

    Added doc, benchmark and deps/X node core PR labels (#32)
    * Added `doc` and `benchmark` node core auto labels
    
    * Added `deps/X` node core auto labels
    
    * Avoid c++ label when only src/node_version.h is affected

--- test/node-labels.test.js
+++ test/node-labels.test.js
@@ -16,6 +16,40 @@ tap.test('label: "test" when only ./test/ files has been changed', (t) => {
   t.end()
 })
 
+tap.test('no labels: when ./test/ and ./doc/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'test/debugger/test-debugger-pid.js',
+    'doc/api/fs.md'
+  ])
+
+  t.same(labels, [])
+
+  t.end()
+})
+
+tap.test('label: "doc" when only ./doc/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'doc/api/fs.md',
+    'doc/api/http.md',
+    'doc/onboarding.md'
+  ])
+
+  t.same(labels, ['doc'])
+
+  t.end()
+})
+
+tap.test('label: "benchmark" when only ./benchmark/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'benchmark/http_server_lag.js',
+    'benchmark/http/check_is_http_token.js'
+  ])
+
+  t.same(labels, ['benchmark'])
+
+  t.end()
+})
+
 tap.test('label: "c++" when ./src/* has been changed', (t) => {
   const labels = nodeLabels.resolveLabels([
     'src/async-wrap.h',
@@ -23,6 +57,47 @@ tap.test('label: "c++" when ./src/* has been changed', (t) => {
   ])
 
   t.same(labels, ['c++'])
+
+  t.end()
+})
+
+tap.test('label: not "c++" when ./src/node_version.h has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'src/node_version.h'
+  ])
+
+  t.same(labels, [])
+
+  t.end()
+})
+
+tap.test('label: "v8" when ./deps/v8/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'deps/v8/src/arguments.cc'
+  ])
+
+  t.same(labels, ['v8'])
+
+  t.end()
+})
+
+tap.test('label: "libuv" when ./deps/ub/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'deps/uv/src/fs-poll.c'
+  ])
+
+  t.same(labels, ['libuv'])
+
+  t.end()
+})
+
+tap.test('label: "v8", "openssl" when ./deps/v8/ and ./deps/openssl/ files has been changed', (t) => {
+  const labels = nodeLabels.resolveLabels([
+    'deps/v8/src/arguments.cc',
+    'deps/openssl/openssl/ssl/ssl_rsa.c'
+  ])
+
+  t.same(labels, ['v8', 'openssl'])
 
   t.end()
 })
