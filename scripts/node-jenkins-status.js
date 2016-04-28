15f4b71d72f5ea6bef99386cbf5d7e99e1a70b43
Phillip Johnsen <johphi@gmail.com>
2016-04-28 21:55:16 +0200

    First shot at polling Jenkins for PR/build status

---
+++ scripts/node-jenkins-status.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const pollJenkins = require('../lib/poll-jenkins')
+
+module.exports = function (app) {
+
+  // to trigger polling manually
+  app.get('/jenkins/:prId', (req, res) => {
+    const prId = req.params.prId
+
+    pollJenkins({
+      owner: 'nodejs',
+      repoName: 'node',
+      prId: parseInt(prId, 10)
+    })
+
+    res.end()
+  })
+}
