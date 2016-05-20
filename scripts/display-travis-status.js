nodejs/github-bot

commit d5fdb5d5a5a6c9705e32524efdb4530f390e73b8

Expose traceable logs (#42)
* Replaced console.log/.error/.warn -> bunyan
* Using bunyan-middleware for unique loggers per request for traceability
* Only FATAL logs when running tests
* Serving static files in ./logs by user/pass
* Configurable num of days to keep logs

scripts/display-travis-status.js
@@ -9,22 +9,24 @@ module.exports = function (app) {
   // Pull Request updates
   app.on('pull_request.synchronize', handlePrUpdate)
 
-  function handlePrUpdate (event) {
-    const owner = event.repository.owner.login
-    const repo = event.repository.name
+  function handlePrUpdate (event, owner, repo, pr) {
     if (!~enabledRepos.indexOf(repo)) return
 
-    debug(`/${owner}/${repo}/pull/${event.number} opened`)
-    pollTravis.pollThenStatus(owner, repo, event.number)
+    const options = { owner, repo, pr, logger: event.logger }
+
+    debug(`/${owner}/${repo}/pull/${pr} opened`)
+    pollTravis.pollThenStatus(options)
   }
 
   // to trigger polling manually
   app.get('/pr/:owner/:repo/:id', (req, res) => {
     const owner = req.params.owner
     const repo = req.params.repo
-    const id = req.params.id
+    const pr = parseInt(req.params.id, 10)
+    const options = { owner, repo, pr, logger: req.log }
+
     if (~enabledRepos.indexOf(repo)) {
-      pollTravis.pollThenStatus(owner, repo, parseInt(id, 10))
+      pollTravis.pollThenStatus(options)
     }
     res.end()
   })

app.js
@@ -2,19 +2,30 @@
 
 require('dotenv').load({ silent: true })
 
+const path = require('path')
 const glob = require('glob')
 const express = require('express')
 const bodyParser = require('body-parser')
+const bunyanMiddleware = require('bunyan-middleware')
+
+const logger = require('./lib/logger')
+const authMiddleware = require('./lib/auth-middleware')
 
 const captureRaw = (req, res, buffer) => { req.raw = buffer }
 
 const app = express()
+
 app.use(bodyParser.json({ verify: captureRaw }))
+app.use('/logs', authMiddleware, express.static(path.join(__dirname, 'logs')))
+// bunyanMiddleware gives us request id's and unique loggers per incoming request,
+// for satefy reasons we don't want to include the webhook GitHub secret in logs
+app.use(bunyanMiddleware({ logger, obscureHeaders: ['x-hub-signature'] }))
+
 require('./lib/github-events')(app)
 
 // load all the files in the scripts folder
 glob.sync(process.argv[2] || './scripts/**/*.js').forEach((file) => {
-  console.log('loading:', file)
+  logger.info('Loading:', file)
   require(file)(app)
 })
 

lib/auth-middleware.js
@@ -1,0 +1,26 @@
+'use strict'
+
+/**
+ * Routes using this middleware gets HTTP basic authentication.
+ * There's only support for *one* username and password combination,
+ * which is set in the $LOGIN_CREDENTIALS environment variable
+ * in the follow format: "username:password"
+ */
+
+const auth = require('basic-auth')
+
+const pkg = require('../package')
+
+const [ username, password ] = (process.env.LOGIN_CREDENTIALS || '').split(':')
+
+module.exports = function authMiddleware (req, res, next) {
+  const user = auth(req)
+
+  if (user === undefined || user['name'] !== username || user['pass'] !== password) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', `Basic realm="${pkg.name}"`)
+    res.end('Unauthorized')
+  } else {
+    next()
+  }
+}

lib/github-events.js
@@ -22,22 +22,28 @@ module.exports = (app) => {
 
     if (!signature || signature !== sign(secret, req.raw)) {
       res.writeHead(401, 'Invalid Signature')
-      console.error('! event@%s: %s - invalid signature, returning 401', data.repository.full_name, data.action)
+      req.log.error('Invalid GitHub event signature, returning 401')
       return res.end()
     }
 
     res.end()
 
-    app.emitGhEvent(data)
+    app.emitGhEvent(data, req.log)
   })
 
-  app.emitGhEvent = function emitGhEvent (data) {
-    const repo = data.repository
-    const org = repo.owner.login || data.organization.login
+  app.emitGhEvent = function emitGhEvent (data, logger) {
+    const repo = data.repository.name
+    const org = data.repository.owner.login || data.organization.login
+    const pr = data.number
 
-    console.log('event@%s/%s: %s', org, repo.name, data.action)
+    // create unique logger which is easily traceable throughout the entire app
+    // by having e.g. "nodejs/nodejs.org/#1337" part of every subsequent log statement
+    const prTrace = `${org}/${repo}/#${pr}`
+    data.logger = logger.child({ pr: prTrace, action: data.action }, true)
+
+    data.logger.info('Emitting GitHub event')
     debug(data)
 
-    app.emit(data.action, data, org, repo.name, data.sender.login)
+    app.emit(data.action, data, org, repo, data.sender.login)
   }
 }

lib/logger.js
@@ -1,0 +1,25 @@
+'use strict'
+
+const path = require('path')
+const bunyan = require('bunyan')
+
+const isRunningTests = process.env.npm_lifecycle_event === 'test'
+const stdoutLevel = isRunningTests ? 'FATAL' : 'INFO'
+
+const daysToKeepLogs = process.env.KEEP_LOGS || 10
+
+module.exports = bunyan.createLogger({
+  name: 'bot',
+  streams: [
+    {
+      stream: process.stdout,
+      level: stdoutLevel
+    },
+    {
+      type: 'rotating-file',
+      path: path.join(__dirname, '../logs/bot.log'),
+      period: '1d', // daily rotation
+      count: daysToKeepLogs
+    }
+  ]
+})

lib/node-repo.js
@@ -11,7 +11,7 @@ function resolveLabelsThenUpdatePr (options) {
     number: options.prId
   }, (err, res) => {
     if (err) {
-      return console.error(`! ${prInfoStr(options)} Error retrieving files`, err)
+      return options.logger.error(err, 'Error retrieving files from GitHub')
     }
 
     const filepathsChanged = res.map((fileMeta) => fileMeta.filename)
@@ -39,10 +39,10 @@ function updatePrWithLabels (options, labels) {
       labels: mergedLabels
     }, (err) => {
       if (err) {
-        return console.error(`! ${prInfoStr(options)} Error while adding labels`, err)
+        return options.logger.error(err, 'Error while editing issue to add labels')
       }
 
-      console.log(`! ${prInfoStr(options)} Added labels: ${labels}`)
+      options.logger.info(`Added labels: ${labels}`)
     })
   })
 }
@@ -54,17 +54,13 @@ function fetchExistingLabels (options, cb) {
     number: options.prId
   }, (err, res) => {
     if (err) {
-      console.error(`! ${prInfoStr(options)} Error while fetching existing labels`, err)
+      options.logger.error(err, 'Error while fetching existing issue labels')
       return cb(err)
     }
 
     const existingLabels = res.map((labelMeta) => labelMeta.name)
     cb(null, existingLabels)
   })
-}
-
-function prInfoStr (options) {
-  return `${options.owner}/${options.repo}/#${options.prId}`
 }
 
 exports.resolveLabelsThenUpdatePr = resolveLabelsThenUpdatePr

lib/pollTravis.js
@@ -14,16 +14,14 @@ exports.pollThenStatus = pollByCommitThenStatus
 /**
  * Poll and match builds by the last commit SHA of the related PR.
  */
-function pollByCommitThenStatus (owner, repoName, prId) {
-  const prInfo = prInfoStr({ owner, repoName, prId })
-
+function pollByCommitThenStatus (options) {
   githubClient.pullRequests.getCommits({
-    user: owner,
-    repo: repoName,
-    number: prId
+    user: options.owner,
+    repo: options.repo,
+    number: options.pr
   }, (err, commitMetas) => {
     if (err) {
-      return console.error(`! ${prInfo} Got error when retrieving GitHub commits for PR`, err.stack)
+      return options.logger.error(err, 'Got error when retrieving GitHub commits for PR')
     }
 
     const lastCommitMeta = commitMetas.pop()
@@ -32,40 +30,43 @@ function pollByCommitThenStatus (owner, repoName, prId) {
       date: lastCommitMeta.commit.committer.date
     }
 
-    pollTravisBuildBySha({ owner, repoName, prId, lastCommit })
-    console.log(`* ${prInfo} Started polling Travis for build by commit ${lastCommit.sha.substr(0, 7)}`)
+    const optsWithCommit = Object.assign({ lastCommit }, options)
+
+    // attach last commit sha to all subsequent logs
+    optsWithCommit.logger = options.logger.child({ commit: lastCommit.sha }, true)
+
+    pollTravisBuildBySha(optsWithCommit)
+    optsWithCommit.logger.info('Started polling Travis for build by commit')
   })
 }
 
-function pollTravisBuildBySha (options, checkNumber) {
+function pollTravisBuildBySha (options, checkNumber = 1) {
   const createGhStatus = createGhStatusFn(options)
-  const prInfo = prInfoStr(options)
   const toMatch = {
     sha: options.lastCommit.sha,
     date: options.lastCommit.date,
-    prId: options.prId
+    pr: options.pr
   }
 
-  checkNumber = checkNumber || 1
-
   if (checkNumber > 100) {
-    console.warn(`* ${prInfo} Was not able to find matching build for PR, stopping poll now :(`)
+    options.logger.warn('Was not able to find matching build for PR, stopping poll now :(')
     return
   }
 
-  travisClient.repos(options.owner, options.repoName).builds.get((err, res) => {
+  travisClient.repos(options.owner, options.repo).builds.get((err, res) => {
     if (err) {
-      return console.error(`! ${prInfo} Got error when retrieving Travis builds`, err.stack)
+      return options.logger.error(err, 'Got error when retrieving Travis builds')
     }
 
     const matchingCommit = res.commits.find((commit) => {
-      const matchesShaOrPr = commit.sha === toMatch.sha || commit.pull_request_number === toMatch.prId
+      const matchesShaOrPr = commit.sha === toMatch.sha || commit.pull_request_number === toMatch.pr
       const matchesCommitDate = commit.committed_at === toMatch.date
 
       return matchesCommitDate && matchesShaOrPr
     })
+
     if (!matchingCommit) {
-      console.warn(`! ${prInfo} Travis hasn't picked up last commit yet, will do check #${checkNumber + 1} in 30 seconds`)
+      options.logger.warn(`Travis hasn't picked up last commit yet, will do check #${checkNumber + 1} in 30 seconds`)
       return setTimeout(pollTravisBuildBySha, 30 * 1000, options, checkNumber + 1)
     }
 
@@ -78,13 +79,13 @@ function pollTravisBuildBySha (options, checkNumber) {
       } else if (lastState === 'failed') {
         return createGhStatus('failure', lastBuildForCommit.id, 'build failure')
       } else if (~['created', 'started'].indexOf(lastState)) {
-        console.log(`* ${prInfo} "${lastState}" build found, will do check #${checkNumber + 1} in 30 seconds`)
+        options.logger.info(`"${lastState}" build found, will do check #${checkNumber + 1} in 30 seconds`)
         createGhStatus('pending', lastBuildForCommit.id, 'build in progress')
       } else {
-        return console.log(`* ${prInfo} Unknown build state: "${lastState}", stopping polling`)
+        return options.logger.info(`Unknown build state: "${lastState}", stopping polling`)
       }
     } else {
-      console.warn(`! ${prInfo} Was not able to find matching build by last commit, will do check #${checkNumber + 1} in 30 seconds`)
+      options.logger.warn(`Was not able to find matching build by last commit, will do check #${checkNumber + 1} in 30 seconds`)
     }
 
     setTimeout(pollTravisBuildBySha, 30 * 1000, options, checkNumber + 1)
@@ -92,26 +93,20 @@ function pollTravisBuildBySha (options, checkNumber) {
 }
 
 function createGhStatusFn (options) {
-  const prInfo = prInfoStr(options)
-
   return (state, travisId, message) => {
     githubClient.statuses.create({
       user: options.owner,
-      repo: options.repoName,
+      repo: options.repo,
       sha: options.lastCommit.sha,
-      target_url: `https://travis-ci.org/${options.owner}/${options.repoName}/builds/${travisId}`,
+      target_url: `https://travis-ci.org/${options.owner}/${options.repo}/builds/${travisId}`,
       context: 'Travis CI via nodejs-github-bot',
       state: state,
       description: message
     }, (err, res) => {
       if (err) {
-        return console.error(`! ${prInfo} Error while updating GitHub PR status`, err)
+        return options.logger.error(err, 'Error while updating GitHub PR status')
       }
-      console.log(`* ${prInfo} Github PR status updated`)
+      options.logger.info('Github PR status updated')
     })
   }
-}
-
-function prInfoStr (options) {
-  return `${options.owner}/${options.repoName}/#${options.prId}`
 }

lib/push-jenkins-update.js
@@ -3,44 +3,36 @@
 const githubClient = require('./github-client')
 
 function push (options, build) {
-  const statusOpts = extendWithBuildData(options, build)
-  const createGhStatus = createGhStatusFn(statusOpts)
+  // create unique logger which is easily traceable for every subsequent log statement
+  const traceFields = { commit: build.commit, job: build.identifier, status: build.status }
+  const logger = options.logger.child(traceFields, true)
 
-  createGhStatus(build.status, build.message)
-}
-
-function createGhStatusFn (options) {
-  const prInfo = prInfoStr(options)
-
-  return (state, message) => {
-    githubClient.statuses.create({
-      user: options.owner,
-      repo: options.repoName,
-      sha: options.sha,
-      target_url: options.url,
-      context: options.context,
-      state: state,
-      description: message
-    }, (err, res) => {
-      if (err) {
-        return console.error(`! ${prInfo} Error while updating Jenkins / GitHub PR status to '${state}' for ${options.context}`, err)
-      }
-      console.log(`* ${prInfo} Jenkins / Github PR status updated to '${state}' for ${options.context}`)
-    })
-  }
-}
-
-function extendWithBuildData (options, build) {
-  return Object.assign({
+  const statusOpts = Object.assign({
     sha: build.commit,
     url: build.url,
-    context: build.identifier
+    context: build.identifier,
+    state: build.status,
+    message: build.message
   }, options)
+
+  createGhStatus(statusOpts, logger)
 }
 
-function prInfoStr (options) {
-  const shortSha = options.sha.substr(0, 7)
-  return `${options.owner}/${options.repoName}/${shortSha}`
+function createGhStatus (options, logger) {
+  githubClient.statuses.create({
+    user: options.owner,
+    repo: options.repo,
+    sha: options.sha,
+    target_url: options.url,
+    context: options.context,
+    state: options.state,
+    description: options.message
+  }, (err, res) => {
+    if (err) {
+      return logger.error(err, 'Error while updating Jenkins / GitHub PR status')
+    }
+    logger.info('Jenkins / Github PR status updated')
+  })
 }
 
 function validate (payload) {

commit e6f0d34c17330f6383e1de0dfdd80c99013b1d22

Fix invalid PR argument in Travis poll job.
Seen in the logs:

```
12:18:09.203 ERROR bot: Got error when retrieving GitHub commits for PR (req_id=.., pr=nodejs/nodejs.org/#754, action=pull_request.opened)
    err: {
      "code": "400",
      "status": "Bad Request",
      "message": "Invalid value for parameter 'number': lpinca"
    }
```

Bug was introduced in d5fdb5d5a5a6c9705e32524efdb4530f390e73b8.

scripts/display-travis-status.js
@@ -9,9 +9,10 @@ module.exports = function (app) {
   // Pull Request updates
   app.on('pull_request.synchronize', handlePrUpdate)
 
-  function handlePrUpdate (event, owner, repo, pr) {
+  function handlePrUpdate (event, owner, repo) {
     if (!~enabledRepos.indexOf(repo)) return
 
+    const pr = event.number
     const options = { owner, repo, pr, logger: event.logger }
 
     debug(`/${owner}/${repo}/pull/${pr} opened`)