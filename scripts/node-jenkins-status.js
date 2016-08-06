b6585541062508934878af158fe1b7be88431e2e
Phillip Johnsen <johphi@gmail.com>
2016-08-06 12:21:37 +0200

    jenkins: finding commit SHA and new endpoints
    This introduces the bot finding the correct commit SHAs to use
    for core PRs, rather than trusting Jenkins to provide SHAs,
    mainly due to rebasing we often do when running PR jobs.
    
    Also refactored the endpoint Jenkins pushes updates from
    one single endpoint to /node/jenkins/{start,end}

--- scripts/node-jenkins-status.js
+++ scripts/node-jenkins-status.js
@@ -3,14 +3,30 @@
 const pushJenkinsUpdate = require('../lib/push-jenkins-update')
 
 module.exports = function (app) {
-  app.post('/node/jenkins', (req, res) => {
+  app.post('/node/jenkins/start', (req, res) => {
     const isValid = pushJenkinsUpdate.validate(req.body)
 
     if (!isValid) {
       return res.status(400).end('Invalid payload')
     }
 
-    pushJenkinsUpdate.push({
+    pushJenkinsUpdate.pushStarted({
+      owner: 'nodejs',
+      repo: 'node',
+      logger: req.log
+    }, req.body)
+
+    res.status(201).end()
+  })
+
+  app.post('/node/jenkins/end', (req, res) => {
+    const isValid = pushJenkinsUpdate.validate(req.body)
+
+    if (!isValid) {
+      return res.status(400).end('Invalid payload')
+    }
+
+    pushJenkinsUpdate.pushEnded({
       owner: 'nodejs',
       repo: 'node',
       logger: req.log
