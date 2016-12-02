a6c8130d67823bf0624256d57c36369a98c02d32
Teddy Katz <teddy.katz@gmail.com>
2016-12-02 02:08:05 -0500

    labels: fetch multiple pages of labels if necessary (#98)
    Update `fetchExistingLabels` to fetch *all* labels from GitHub, rather
    than the first 100 labels. This allows the bot to work correctly now
    that the repo has more than 100 labels.
    
    Refs: https://github.com/nodejs/node/issues/9841

--- test/unit/node-repo.test.js
+++ test/unit/node-repo.test.js
@@ -10,53 +10,85 @@ const githubClient = require('../../lib/github-client')
 const readFixture = require('../read-fixture')
 
 tap.test('fetchExistingLabels(): caches existing repository labels', (t) => {
-  const fakeGithubClient = sinon.stub(githubClient.issues, 'getLabels').yields(null, [])
+  sinon.stub(githubClient.issues, 'getLabels').yields(null, [])
+  sinon.stub(githubClient, 'hasNextPage', () => false)
   const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': fakeGithubClient
+    './github-client': githubClient
   })
 
   t.plan(1)
-  t.tearDown(() => githubClient.issues.getLabels.restore())
+  t.tearDown(() => {
+    githubClient.issues.getLabels.restore()
+    githubClient.hasNextPage.restore()
+  })
 
   nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, () => {
     nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, () => {
-      t.ok(fakeGithubClient.calledOnce)
+      t.ok(githubClient.issues.getLabels.calledOnce)
     })
   })
 })
 
 tap.test('fetchExistingLabels(): cache expires after one hour', (t) => {
   const clock = lolex.install()
-  const fakeGithubClient = sinon.stub(githubClient.issues, 'getLabels').yields(null, [])
+  sinon.stub(githubClient.issues, 'getLabels').yields(null, [])
+  sinon.stub(githubClient, 'hasNextPage', () => false)
   const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': fakeGithubClient
+    './github-client': githubClient
   })
 
   t.plan(1)
-  t.tearDown(() => githubClient.issues.getLabels.restore() && clock.uninstall())
+  t.tearDown(() => {
+    githubClient.issues.getLabels.restore()
+    githubClient.hasNextPage.restore()
+    clock.uninstall()
+  })
 
   nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, () => {
     // fetch labels again after 1 hour and 1 minute
     clock.tick(1000 * 60 * 61)
 
     nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, () => {
-      t.equal(fakeGithubClient.callCount, 2)
+      t.equal(githubClient.issues.getLabels.callCount, 2)
     })
   })
 })
 
 tap.test('fetchExistingLabels(): yields an array of existing label names', (t) => {
   const labelsFixture = readFixture('repo-labels.json')
-  const fakeGithubClient = sinon.stub(githubClient.issues, 'getLabels').yields(null, labelsFixture)
+  sinon.stub(githubClient.issues, 'getLabels').yields(null, labelsFixture)
+  sinon.stub(githubClient, 'hasNextPage', () => false)
   const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': fakeGithubClient
+    './github-client': githubClient
   })
 
   t.plan(2)
-  t.tearDown(() => githubClient.issues.getLabels.restore())
+  t.tearDown(() => {
+    githubClient.issues.getLabels.restore()
+    githubClient.hasNextPage.restore()
+  })
 
   nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, (err, existingLabels) => {
     t.equal(err, null)
     t.ok(existingLabels.includes('cluster'))
+  })
+})
+
+tap.test('fetchExistingLabels(): can retrieve more than 100 labels', (t) => {
+  const labelsFixturePage1 = readFixture('repo-labels.json')
+  const labelsFixturePage2 = readFixture('repo-labels-page-2.json')
+  sinon.stub(githubClient.issues, 'getLabels', (options, cb) => cb(null, options.page === 1 ? labelsFixturePage1 : labelsFixturePage2))
+  sinon.stub(githubClient, 'hasNextPage', (listing) => listing === labelsFixturePage1)
+  const nodeRepo = proxyquire('../../lib/node-repo', {'./github-client': githubClient})
+
+  t.plan(3)
+  t.tearDown(() => {
+    githubClient.issues.getLabels.restore()
+    githubClient.hasNextPage.restore()
+  })
+  nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger }, (err, existingLabels) => {
+    t.equal(err, null)
+    t.ok(existingLabels.includes('cluster'))
+    t.ok(existingLabels.includes('windows'))
   })
 })
