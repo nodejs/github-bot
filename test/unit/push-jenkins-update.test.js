f701303f5cdd846adee4a1027146853d4c08237f
Phillip Johnsen <johphi@gmail.com>
2017-11-02 08:28:19 +0100

    jenkins: paginate PR commits to find the latest commit (#153)
    When pushing Jenkins status to a PR, we need to reference the commit
    SHA the inline GitHub PR status relates to.
    
    These changes fixes a bug when handling enormous PRs with more than
    100 commits, where we have to paginate to commits results for that PR
    to find the last commit SHA.
    
    Fixes https://github.com/nodejs/github-bot/issues/97

---
+++ test/unit/push-jenkins-update.test.js
@@ -0,0 +1,27 @@
+const tap = require('tap')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+
+const githubClient = require('../../lib/github-client')
+
+const readFixture = require('../read-fixture')
+
+tap.test('findLatestCommitInPr: paginates results when more than 100 commits in a PR', (t) => {
+  const commitsFixturePage1 = readFixture('pull-requests-commits-page-1.json')
+  const commitsFixturePage104 = readFixture('pull-requests-commits-page-104.json')
+
+  sinon.stub(githubClient.pullRequests, 'getCommits', (options, cb) => cb(null, options.page === 1 ? commitsFixturePage1 : commitsFixturePage104))
+  sinon.stub(githubClient, 'hasLastPage', (lastResult) => lastResult === commitsFixturePage1 ? 'https://api.github.com/repos/nodejs/node/pulls/9745/commits?page=104' : undefined)
+  const pushJenkinsUpdate = proxyquire('../../lib/push-jenkins-update', {'./github-client': githubClient})
+
+  t.plan(2)
+
+  pushJenkinsUpdate.findLatestCommitInPr({
+    owner: 'nodejs',
+    repo: 'node',
+    number: 9745
+  }, (err, commit) => {
+    t.equal(err, null)
+    t.equal(commit.sha, 'c1aa949064892dbe693750686c06f4ad5673e577')
+  })
+})
