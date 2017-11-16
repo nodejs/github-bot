af348e5a317115e5fce1b5ff1c7c4b6d804894cc
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-16 19:15:09 +0800

    travis: reduce number of retry and make it configurable

--- scripts/display-travis-status.js
+++ scripts/display-travis-status.js
@@ -2,10 +2,25 @@
 
 const debug = require('debug')('display_travis_status')
 const pollTravis = require('../lib/pollTravis')
-const enabledRepos = [
-  'citgm', 'readable-stream', 'nodejs.org', 'docker-node',
-  'llnode', 'nan', 'node-core-utils', 'core-validate-commit'
-]
+
+// Default is 20 retries, 30 seconds per retry, which is reasonable for
+// smaller projects that finish testing within 10 minutes.
+const DEFAULT_POLL_RETRY = process.env.TRAVIS_POLL_RETRY || 20
+const DEFAULT_POLL_INTERVAL = process.env.TRAVIS_POLL_INTERVAL || 30
+
+// For projects that take longer to build e.g. projects that actually use
+// Travis to build, increase the interval so there will be fewer useless
+// polls
+const enabledRepos = {
+  'citgm': { retry: 40 },
+  'readable-stream': { retry: 60, interval: 60 },
+  'nodejs.org': { },
+  'docker-node': { retry: 70, interval: 120 },
+  'llnode': { retry: 60, interval: 60 },
+  'nan': { retry: 60, interval: 60 },
+  'node-core-utils': { },
+  'core-validate-commit': { }
+}
 
 module.exports = function (app) {
   app.on('pull_request.opened', handlePrUpdate)
@@ -13,10 +28,13 @@ module.exports = function (app) {
   app.on('pull_request.synchronize', handlePrUpdate)
 
   function handlePrUpdate (event, owner, repo) {
-    if (!~enabledRepos.indexOf(repo)) return
+    const config = enabledRepos[repo]
+    if (!config) return
 
     const pr = event.number
-    const options = { owner, repo, pr, logger: event.logger }
+    const retry = config.retry || DEFAULT_POLL_RETRY
+    const interval = config.interval || DEFAULT_POLL_INTERVAL
+    const options = { owner, repo, pr, retry, interval, logger: event.logger }
 
     debug(`/${owner}/${repo}/pull/${pr} opened`)
     pollTravis.pollThenStatus(options)
@@ -27,10 +45,15 @@ module.exports = function (app) {
     const owner = req.params.owner
     const repo = req.params.repo
     const pr = parseInt(req.params.id, 10)
-    const options = { owner, repo, pr, logger: req.log }
-
-    if (~enabledRepos.indexOf(repo)) {
+    const config = enabledRepos[repo]
+    if (config) {
+      const retry = config.retry || DEFAULT_POLL_RETRY
+      const interval = config.interval || DEFAULT_POLL_INTERVAL
+      const options = { owner, repo, pr, retry, interval, logger: req.log }
+      res.writeHead(201)
       pollTravis.pollThenStatus(options)
+    } else {
+      res.writeHead(404, 'Repo not enabled')
     }
     res.end()
   })
