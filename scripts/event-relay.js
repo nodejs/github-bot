9ef8f375fcab480ded9de880116dbd67193250b2
Mary Marchini <mmarchini@netflix.com>
2020-08-02 16:27:18 -0700

    feat: relay jenkins events to github actions
    These changes are meant to allow us to write GitHub Actions in selected
    repositories in the nodejs github organization, as a reaction to
    Jenkins events.
    
    It works by dispatching repository events, which we can use as triggers
    for GitHub Action workflows.
    
    Here's a concrete repository dispatch event caused by the Jenkins job
    `node-test-commit-osx`:
    
    ```
    event_type: jenkins.node-test-commit-osx.start or jenkins.node-test-commit-osx.end
    
    client_payload:
    {
      "identifier": "node-test-commit-osx",
      "status": "success",
      "message": "tests passed",
      "commit": "8a5fec2a6bade91e544a30314d7cf21f8a200de1",
      "url": "https://ci.nodejs.org/job/node-test-commit/3157/",
      "ref": "refs/pull/12345/head"
    }
    
    ```
    
    Refs:
        - https://docs.github.com/en/free-pro-team@latest/actions/reference/events-that-trigger-workflows#repository_dispatch
        - https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-repository-dispatch-event

---
+++ scripts/event-relay.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const githubClient = require('../lib/github-client')
+
+async function handleJenkinsRelay (event) {
+  const { owner, repo, identifier } = event
+  const eventType = `jenkins.${identifier}.${event.event}`
+  try {
+    event.logger.debug(`Relaying ${eventType} to ${owner}/${repo}`)
+    await githubClient.repos.createDispatchEvent({
+      owner,
+      repo,
+      event_type: eventType,
+      client_payload: event
+    })
+    return true
+  } catch (err) {
+    event.logger.error(err, `Failed to relay ${eventType} to ${owner}/${repo}`)
+    return false
+  }
+}
+
+module.exports = function (_, event) {
+  event.on('jenkins', handleJenkinsRelay)
+}
