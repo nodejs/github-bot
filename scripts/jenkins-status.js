5ade7fc64a3ecd53fe4f7daae38603ed6a8370ef
Mary Marchini <mmarchini@netflix.com>
2020-08-02 15:39:50 -0700

    chore(jenkins): refactor jenkins-status using events

--- scripts/jenkins-status.js
+++ scripts/jenkins-status.js
@@ -1,84 +1,36 @@
 'use strict'
 
 const pushJenkinsUpdate = require('../lib/push-jenkins-update')
-const enabledRepos = ['citgm', 'http-parser', 'node']
 
-const jenkinsIpWhitelist = process.env.JENKINS_WORKER_IPS ? process.env.JENKINS_WORKER_IPS.split(',') : []
+function handleJenkinsStart (event) {
+  const { repo, owner } = event
 
-function isJenkinsIpWhitelisted (req) {
-  const ip = req.connection.remoteAddress.split(':').pop()
-
-  if (jenkinsIpWhitelist.length && !jenkinsIpWhitelist.includes(ip)) {
-    req.log.warn({ ip }, 'Ignoring, not allowed to push Jenkins updates')
-    return false
-  }
-
-  return true
+  pushJenkinsUpdate.pushStarted({
+    owner,
+    repo,
+    logger: event.logger
+  }, event, (err) => {
+    if (err) {
+      event.logger.error(err, 'Error while handling Jenkins start event')
+    }
+  })
 }
 
-function isRelatedToPullRequest (gitRef) {
-  // refs/pull/12345/head vs refs/heads/v8.x-staging/head
-  return gitRef.includes('/pull/')
+function handleJenkinsStop (event) {
+  const { repo, owner } = event
+
+  pushJenkinsUpdate.pushEnded({
+    owner,
+    repo,
+    logger: event.logger
+  }, event, (err) => {
+    if (err) {
+      event.logger.error(err, 'Error while handling Jenkins end event')
+    }
+  })
 }
 
-module.exports = function (app) {
-  app.post('/:repo/jenkins/start', (req, res) => {
-    const isValid = pushJenkinsUpdate.validate(req.body)
-    const repo = req.params.repo
-
-    if (!isValid) {
-      return res.status(400).end('Invalid payload')
-    }
-
-    if (!isRelatedToPullRequest(req.body.ref)) {
-      return res.status(400).end('Will only push builds related to pull requests')
-    }
-
-    if (!enabledRepos.includes(repo)) {
-      return res.status(400).end('Invalid repository')
-    }
-
-    if (!isJenkinsIpWhitelisted(req)) {
-      return res.status(401).end('Invalid Jenkins IP')
-    }
-
-    pushJenkinsUpdate.pushStarted({
-      owner: 'nodejs',
-      repo,
-      logger: req.log
-    }, req.body, (err) => {
-      const statusCode = err !== null ? 500 : 201
-      res.status(statusCode).end()
-    })
-  })
-
-  app.post('/:repo/jenkins/end', (req, res) => {
-    const isValid = pushJenkinsUpdate.validate(req.body)
-    const repo = req.params.repo
-
-    if (!isValid) {
-      return res.status(400).end('Invalid payload')
-    }
-
-    if (!isRelatedToPullRequest(req.body.ref)) {
-      return res.status(400).end('Will only push builds related to pull requests')
-    }
-
-    if (!enabledRepos.includes(repo)) {
-      return res.status(400).end('Invalid repository')
-    }
-
-    if (!isJenkinsIpWhitelisted(req)) {
-      return res.status(401).end('Invalid Jenkins IP')
-    }
-
-    pushJenkinsUpdate.pushEnded({
-      owner: 'nodejs',
-      repo,
-      logger: req.log
-    }, req.body, (err) => {
-      const statusCode = err !== null ? 500 : 201
-      res.status(statusCode).end()
-    })
-  })
+module.exports = function (_, event) {
+  event.on('jenkins.start', handleJenkinsStart)
+  event.on('jenkins.end', handleJenkinsStop)
 }
