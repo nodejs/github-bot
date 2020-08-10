41db5cfecc309f1271d9f61bd5a92c629b47c54d
Mary Marchini <mmarchini@netflix.com>
2020-08-09 22:39:23 -0700

    chore: dont use sinon on push-jenkins-update test
    `@octokit/rest` uses deprecation warning to indicate breaking changes,
    and sinon suppresses those warnings when running tests. Replacing sinon
    with nock for testing ensures we fix all deprecation warnings before
    bumping octokit. This commit replaces sinon with nock in one test.

--- test/unit/push-jenkins-update.test.js
+++ test/unit/push-jenkins-update.test.js
@@ -1,27 +1,41 @@
 const tap = require('tap')
-const proxyquire = require('proxyquire')
-const sinon = require('sinon')
 
-const githubClient = require('../../lib/github-client')
+const { findLatestCommitInPr } = require('../../lib/push-jenkins-update')
 
+const nock = require('nock')
 const readFixture = require('../read-fixture')
+const { ignoreQueryParams } = require('../common')
 
 tap.test('findLatestCommitInPr: paginates results when more than 100 commits in a PR', (t) => {
   const commitsFixturePage1 = readFixture('pull-requests-commits-page-1.json')
   const commitsFixturePage104 = readFixture('pull-requests-commits-page-104.json')
+  const owner = 'nodejs'
+  const repo = 'node'
+  const pr = 9745
 
-  sinon.stub(githubClient.pullRequests, 'listCommits', (options, cb) => cb(null, options.page === 1 ? commitsFixturePage1 : commitsFixturePage104))
-  sinon.stub(githubClient, 'hasLastPage', (lastResult) => lastResult === commitsFixturePage1 ? 'https://api.github.com/repos/nodejs/node/pulls/9745/commits?page=104' : undefined)
-  const pushJenkinsUpdate = proxyquire('../../lib/push-jenkins-update', { './github-client': githubClient })
+  const headers = {
+    'Link': '<https://api.github.com/repos/nodejs/node/pulls/9745/commits?page=104>; rel="last"'
+  }
+  const firstPageScope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/pulls/${pr}/commits`)
+    .reply(200, commitsFixturePage1, headers)
+
+  const lastPageScope = nock('https://api.github.com')
+    .get(`/repos/${owner}/${repo}/pulls/${pr}/commits`)
+    .query({ page: 104, per_page: 100, access_token: 'invalid-placeholder-token' })
+    .reply(200, commitsFixturePage104)
 
   t.plan(2)
 
-  pushJenkinsUpdate.findLatestCommitInPr({
-    owner: 'nodejs',
-    repo: 'node',
-    number: 9745
+  findLatestCommitInPr({
+    owner,
+    repo,
+    pr
   }, (err, commit) => {
     t.equal(err, null)
     t.equal(commit.sha, 'c1aa949064892dbe693750686c06f4ad5673e577')
+    firstPageScope.done()
+    lastPageScope.done()
   })
 })
