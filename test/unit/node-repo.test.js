8c8f4914ce9db28081a8349d534252205023165d
Mary Marchini <mmarchini@netflix.com>
2020-08-11 19:36:09 -0700

    chore(test): node-repo replace sinon with nock

--- test/unit/node-repo.test.js
+++ test/unit/node-repo.test.js
@@ -1,118 +1,135 @@
 'use strict'
 
-const proxyquire = require('proxyquire')
-const sinon = require('sinon')
 const tap = require('tap')
 const lolex = require('lolex')
+const nock = require('nock')
+
+const nodeRepo = require('../../lib/node-repo')
 
 const logger = require('../../lib/logger')
-const githubClient = require('../../lib/github-client')
 const readFixture = require('../read-fixture')
+const { ignoreQueryParams } = require('../common')
 
 tap.test('fetchExistingLabels(): caches existing repository labels', async (t) => {
-  sinon.stub(githubClient.issues, 'listLabelsForRepo', () => Promise.resolve([]))
-  sinon.stub(githubClient, 'hasNextPage', () => false)
-  const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': githubClient
-  })
+  const owner = 'nodejs'
+  const repo = 'node1'
+  // Test passes if nock is only called once, no other checks to run
+  const scope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/labels`)
+    .once() // should only be called once
+    .reply(200, [])
 
-  t.plan(1)
-  t.tearDown(() => {
-    githubClient.issues.listLabelsForRepo.restore()
-    githubClient.hasNextPage.restore()
-  })
-
-  await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
-  await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
-  t.ok(githubClient.issues.listLabelsForRepo.calledOnce)
+  await nodeRepo._fetchExistingLabels({ owner, repo, logger })
+  await nodeRepo._fetchExistingLabels({ owner, repo, logger })
+  scope.done()
 })
 
 tap.test('fetchExistingLabels(): cache expires after one hour', async (t) => {
+  const owner = 'nodejs'
+  const repo = 'node2'
   const clock = lolex.install()
-  sinon.stub(githubClient.issues, 'listLabelsForRepo', () => Promise.resolve([]))
-  sinon.stub(githubClient, 'hasNextPage', () => false)
-  const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': githubClient
-  })
 
-  t.plan(1)
+  // Test passes if nock is only called once, no other checks to run
+  const scope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/labels`)
+    .twice() // should be called twice
+    .reply(200, [])
+
   t.tearDown(() => {
-    githubClient.issues.listLabelsForRepo.restore()
-    githubClient.hasNextPage.restore()
     clock.uninstall()
   })
 
-  await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
+  await nodeRepo._fetchExistingLabels({ owner, repo, logger })
 
   // fetch labels again after 1 hour and 1 minute
   clock.tick(1000 * 60 * 61)
 
-  await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
-  t.equal(githubClient.issues.listLabelsForRepo.callCount, 2)
+  await nodeRepo._fetchExistingLabels({ owner, repo, logger })
+  scope.done()
 })
 
 tap.test('fetchExistingLabels(): yields an array of existing label names', async (t) => {
   const labelsFixture = readFixture('repo-labels.json')
-  sinon.stub(githubClient.issues, 'listLabelsForRepo', () => Promise.resolve(labelsFixture))
-  sinon.stub(githubClient, 'hasNextPage', () => false)
-  const nodeRepo = proxyquire('../../lib/node-repo', {
-    './github-client': githubClient
-  })
+  const owner = 'nodejs'
+  const repo = 'node3'
+
+  const scope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/labels`)
+    .reply(200, labelsFixture.data)
 
   t.plan(1)
-  t.tearDown(() => {
-    githubClient.issues.listLabelsForRepo.restore()
-    githubClient.hasNextPage.restore()
-  })
 
-  const existingLabels = await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
+  const existingLabels = await nodeRepo._fetchExistingLabels({ owner, repo, logger })
   t.ok(existingLabels.includes('cluster'))
+  scope.done()
 })
 
 tap.test('fetchExistingLabels(): can retrieve more than 100 labels', async (t) => {
   const labelsFixturePage1 = readFixture('repo-labels.json')
   const labelsFixturePage2 = readFixture('repo-labels-page-2.json')
-  sinon.stub(githubClient.issues, 'listLabelsForRepo', (options) => Promise.resolve(options.page === 1 ? labelsFixturePage1 : labelsFixturePage2))
-  sinon.stub(githubClient, 'hasNextPage', (listing) => listing === labelsFixturePage1)
-  const nodeRepo = proxyquire('../../lib/node-repo', { './github-client': githubClient })
+  const owner = 'nodejs'
+  const repo = 'node4'
+  const headers = {
+    'Link': `<https://api.github.com/repos/${owner}/${repo}/labels?page=2>; rel="next"`
+  }
+
+  const firstPageScope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/labels`)
+    .reply(200, labelsFixturePage1.data, headers)
+
+  const secondPageScope = nock('https://api.github.com')
+    .get(`/repos/${owner}/${repo}/labels`)
+    .query({ page: 2, per_page: 100, access_token: 'invalid-placeholder-token' })
+    .reply(200, labelsFixturePage2.data)
 
   t.plan(2)
-  t.tearDown(() => {
-    githubClient.issues.listLabelsForRepo.restore()
-    githubClient.hasNextPage.restore()
-  })
 
-  const existingLabels = await nodeRepo._fetchExistingLabels({ owner: 'nodejs', repo: 'node', logger })
+  const existingLabels = await nodeRepo._fetchExistingLabels({ owner, repo, logger })
   t.ok(existingLabels.includes('cluster'))
   t.ok(existingLabels.includes('windows'))
+  firstPageScope.done()
+  secondPageScope.done()
 })
 
 tap.test('getBotPrLabels(): returns labels added by nodejs-github-bot', (t) => {
   const events = readFixture('pull-request-events.json')
-  sinon.stub(githubClient.issues, 'getEvents', (options, cb) => { cb(null, events) })
-  const nodeRepo = proxyquire('../../lib/node-repo', { './github-client': githubClient })
+
+  const owner = 'nodejs'
+  const repo = 'node5'
+  const prId = '1'
+
+  const scope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/issues/${prId}/events`)
+    .reply(200, events.data)
 
   t.plan(1)
-  t.tearDown(() => {
-    githubClient.issues.getEvents.restore()
-  })
 
-  nodeRepo.getBotPrLabels({ owner: 'nodejs', repo: 'node', prId: '1' }, (_, labels) => {
+  nodeRepo.getBotPrLabels({ owner, repo, prId }, (_, labels) => {
     t.same(labels, ['testlabel'])
+    scope.done()
   })
 })
 
 tap.test('getBotPrLabels(): returns net labels added/removed by nodejs-github-bot', (t) => {
   const events = readFixture('pull-request-events-2.json')
-  sinon.stub(githubClient.issues, 'getEvents', (options, cb) => { cb(null, events) })
-  const nodeRepo = proxyquire('../../lib/node-repo', { './github-client': githubClient })
 
+  const owner = 'nodejs'
+  const repo = 'node6'
+  const prId = '2'
+
+  const scope = nock('https://api.github.com')
+    .filteringPath(ignoreQueryParams)
+    .get(`/repos/${owner}/${repo}/issues/${prId}/events`)
+    .reply(200, events.data)
   t.plan(1)
-  t.tearDown(() => {
-    githubClient.issues.getEvents.restore()
-  })
 
-  nodeRepo.getBotPrLabels({ owner: 'nodejs', repo: 'node', prId: '1' }, (_, labels) => {
+  nodeRepo.getBotPrLabels({ owner, repo, prId }, (_, labels) => {
     t.same(labels, [])
+    scope.done()
   })
 })
