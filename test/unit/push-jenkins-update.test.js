618dda1c502cda14b34d3ff5f7b0638e753b6bed
Mary Marchini <mmarchini@netflix.com>
2020-08-15 00:01:56 -0700

    chore: async findLatestCommitInPr

--- test/unit/push-jenkins-update.test.js
+++ test/unit/push-jenkins-update.test.js
@@ -6,7 +6,7 @@ const nock = require('nock')
 const readFixture = require('../read-fixture')
 const { ignoreQueryParams } = require('../common')
 
-tap.test('findLatestCommitInPr: paginates results when more than 100 commits in a PR', (t) => {
+tap.test('findLatestCommitInPr: paginates results when more than 100 commits in a PR', async (t) => {
   const commitsFixturePage1 = readFixture('pull-requests-commits-page-1.json')
   const commitsFixturePage104 = readFixture('pull-requests-commits-page-104.json')
   const owner = 'nodejs'
@@ -26,16 +26,10 @@ tap.test('findLatestCommitInPr: paginates results when more than 100 commits in 
     .query({ page: 104, per_page: 100, access_token: 'invalid-placeholder-token' })
     .reply(200, commitsFixturePage104)
 
-  t.plan(2)
+  t.plan(1)
 
-  findLatestCommitInPr({
-    owner,
-    repo,
-    pr
-  }, (err, commit) => {
-    t.equal(err, null)
-    t.equal(commit.sha, 'c1aa949064892dbe693750686c06f4ad5673e577')
-    firstPageScope.done()
-    lastPageScope.done()
-  })
+  const commit = await findLatestCommitInPr({ owner, repo, pr })
+  t.equal(commit.sha, 'c1aa949064892dbe693750686c06f4ad5673e577')
+  firstPageScope.done()
+  lastPageScope.done()
 })
