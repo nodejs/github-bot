nodejs/github-bot

commit 7b4ac859952163479b2afe159cf0ff29416b37bb

chore: replace deprecated Octokit number option
Wherever we're providing Octokit with the issue / PR numbers, have been
raising deprecation warnings lately, stating we should use
`pull_number | issue_number` instead of the previous `number` option the
majority of Octokit methods accepts.

```
{ Deprecation: [@octokit/rest] "number" parameter is deprecated for ".issues.createComment()". Use "issue_number" instead
    at forEach (/home/travis/build/nodejs/github-bot/node_modules/@octokit/plugin-rest-endpoint-methods/dist-src/register-endpoints.js:47:34)
    at Array.forEach (<anonymous>)
    at Object.createComment (/home/travis/build/nodejs/github-bot/node_modules/@octokit/plugin-rest-endpoint-methods/dist-src/register-endpoints.js:44:30)
    at createPrComment (/home/travis/build/nodejs/github-bot/lib/github-comment.js:7:31)
    at Object.pushStarted (/home/travis/build/nodejs/github-bot/lib/push-jenkins-update.js:20:9)
```

lib/node-repo.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* eslint-disable camelcase */
+
 const LRU = require('lru-cache')
 const Aigle = require('aigle')
 const request = require('request')
@@ -28,7 +30,7 @@ async function resolveLabelsThenUpdatePr (options) {
   const filepathsChanged = await retry(() => listFiles({
     owner: options.owner,
     repo: options.repo,
-    number: options.prId,
+    pull_number: options.prId,
     logger: options.logger
   }))
   options.logger.debug('Fetching PR files for labelling')
@@ -64,7 +66,7 @@ async function updatePrWithLabels (options, labels) {
     await githubClient.issues.addLabels({
       owner: options.owner,
       repo: options.repo,
-      number: options.prId,
+      issue_number: options.prId,
       labels: labels
     })
 
@@ -86,7 +88,7 @@ async function removeLabelFromPR (options, label) {
     await githubClient.issues.removeLabel({
       owner: options.owner,
       repo: options.repo,
-      number: options.prId,
+      issue_number: options.prId,
       name: label
     })
   } catch (err) {
@@ -148,7 +150,7 @@ function getBotPrLabels (options, cb) {
     repo: options.repo,
     page: 1,
     per_page: 100, // we probably won't hit this
-    number: options.prId
+    issue_number: options.prId
   }).then(res => {
     const events = res.data || []
     const ourLabels = []
@@ -192,12 +194,12 @@ function getCodeOwnersUrl (owner, repo, defaultBranch) {
   return `https://${base}/${owner}/${repo}/${defaultBranch}/${filepath}`
 }
 
-async function listFiles ({ owner, repo, number, logger }) {
+async function listFiles ({ owner, repo, pull_number, logger }) {
   try {
     const response = await githubClient.pulls.listFiles({
       owner,
       repo,
-      number
+      pull_number
     })
     return response.data.map(({ filename }) => filename)
   } catch (err) {
@@ -274,7 +276,7 @@ async function pingOwners (options, owners) {
     await createPrComment({
       owner: options.owner,
       repo: options.repo,
-      number: options.prId,
+      issue_number: options.prId,
       logger: options.logger
     }, getCommentForOwners(owners))
   } catch (err) {

lib/github-comment.js
@@ -1,13 +1,15 @@
 'use strict'
 
+/* eslint-disable camelcase */
+
 const githubClient = require('./github-client')
 
-exports.createPrComment = async function createPrComment ({ owner, repo, number, logger }, body) {
+exports.createPrComment = async function createPrComment ({ owner, repo, issue_number, logger }, body) {
   try {
     await githubClient.issues.createComment({
       owner,
       repo,
-      number,
+      issue_number,
       body
     })
   } catch (err) {

lib/push-jenkins-update.js
@@ -17,11 +17,11 @@ function pushStarted (options, build, cb) {
   if (build.status === 'pending') {
     switch (build.identifier) {
       case 'node-test-pull-request':
-        createPrComment(Object.assign({ number: pr }, options), `CI: ${build.url}`)
+        createPrComment(Object.assign({ issue_number: pr }, options), `CI: ${build.url}`)
         break
 
       case 'node-test-pull-request-lite-pipeline':
-        createPrComment(Object.assign({ number: pr }, options), `Lite-CI: ${build.url}`)
+        createPrComment(Object.assign({ issue_number: pr }, options), `Lite-CI: ${build.url}`)
         break
 
       default:
@@ -79,7 +79,7 @@ async function findLatestCommitInPr (options, pageNumber = 1) {
   const res = await githubClient.pulls.listCommits({
     owner: options.owner,
     repo: options.repo,
-    number: options.pr,
+    pull_number: options.pr,
     page: pageNumber,
     per_page: 100
   })

test/unit/node-repo-owners.test.js
@@ -19,7 +19,7 @@ const options = {
   owner: 'nodejs',
   repo: 'node-auto-test',
   prId: 12345,
-  number: 12345,
+  pull_number: 12345,
   logger: { info: () => {}, error: () => {}, debug: () => {}, child: function () { return this } },
   retries: 1,
   defaultBranch: 'main',

commit 9d965655336e449d703ca58b76171e49fbbaed67

fix(owners): adjust list files function using correct options for PR #
To fix the nodejs/node PR owners ping functionality, these changes
adjusts the internal helper function used to list affected files in a PR
to ensure it provides the correct pull request number.

Fixes bug originally introduced in https://github.com/nodejs/github-bot/commit/7b4ac859952163479b2afe159cf0ff29416b37bb
when `.number -> .pull_number` changes was introduced. It broke the
said helper function, which at that time expected an object containing
`.pull_number` provided as argument, but ended up getting the GitHub
webhook event `options` provided, where that PR number is represented in
`.prId`.

lib/node-repo.js
@@ -84,12 +84,12 @@ function getCodeOwnersUrl (owner, repo, defaultBranch) {
   return `https://${base}/${owner}/${repo}/${defaultBranch}/${filepath}`
 }
 
-async function listFiles ({ owner, repo, pull_number, logger }) {
+async function listFiles ({ owner, repo, prId, logger }) {
   try {
     const response = await githubClient.pulls.listFiles({
       owner,
       repo,
-      pull_number
+      pull_number: prId
     })
     return response.data.map(({ filename }) => filename)
   } catch (err) {
@@ -133,7 +133,6 @@ async function resolveOwnersThenPingPr (options) {
   const retry = fn => Aigle.retry({ times, interval }, fn)
 
   options.logger.debug('Getting file paths')
-  options.number = options.prId
   const filepathsChanged = await retry(() => listFiles(options))
   options.logger = options.logger.child({ filepathsChanged })
 