ad3903c4fb9b7d88212f234d59f5d87d4aee5624
Rich Trott <rtrott@gmail.com>
2023-03-02 13:29:35 -0800

    chore: update @octokit/rest from v16 to v17 (#371)
    * chore: replace deprecated .hasLastPage() with pagination
    
    This is a prerequisite for updating @octokit/rest.
    
    * chore: update @octokit/rest from v16 to v17

--- test/unit/push-jenkins-update.test.js
+++ test/unit/push-jenkins-update.test.js
@@ -4,32 +4,41 @@ const { findLatestCommitInPr } = require('../../lib/push-jenkins-update')
 
 const nock = require('nock')
 const readFixture = require('../read-fixture')
-const { ignoreQueryParams } = require('../common')
 
 tap.test('findLatestCommitInPr: paginates results when more than 100 commits in a PR', async (t) => {
-  const commitsFixturePage1 = readFixture('pull-requests-commits-page-1.json')
-  const commitsFixturePage104 = readFixture('pull-requests-commits-page-104.json')
+  const commitsFixturePage1 = readFixture('pull-request-commits-page-1.json')
+  const commitsFixturePage2 = readFixture('pull-request-commits-page-2.json')
+  const commitsFixturePage3 = readFixture('pull-request-commits-page-3.json')
+  const commitsFixturePage4 = readFixture('pull-request-commits-page-4.json')
   const owner = 'nodejs'
   const repo = 'node'
   const pr = 9745
 
-  const headers = {
-    Link: '<https://api.github.com/repos/nodejs/node/pulls/9745/commits?page=104>; rel="last"'
-  }
   const firstPageScope = nock('https://api.github.com')
-    .filteringPath(ignoreQueryParams)
     .get(`/repos/${owner}/${repo}/pulls/${pr}/commits`)
-    .reply(200, commitsFixturePage1, headers)
+    .reply(200, commitsFixturePage1, { link: '<https://api.github.com/repositories/27193779/pulls/9745/commits?page=2>; rel="next", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=4>; rel="last"' })
 
-  const lastPageScope = nock('https://api.github.com')
-    .get(`/repos/${owner}/${repo}/pulls/${pr}/commits`)
-    .query({ page: 104, per_page: 100 })
-    .reply(200, commitsFixturePage104)
+  const secondPageScope = nock('https://api.github.com')
+    .get(`/repositories/27193779/pulls/${pr}/commits`)
+    .query({ page: 2 })
+    .reply(200, commitsFixturePage2, { link: '<https://api.github.com/repositories/27193779/pulls/9745/commits?page=1>; rel="prev", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=3>; rel="next", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=4>; rel="last", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=1>; rel="first"' })
+
+  const thirdPageScope = nock('https://api.github.com')
+    .get(`/repositories/27193779/pulls/${pr}/commits`)
+    .query({ page: 3 })
+    .reply(200, commitsFixturePage3, { link: '<https://api.github.com/repositories/27193779/pulls/9745/commits?page=2>; rel="prev", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=4>; rel="next", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=4>; rel="last", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=1>; rel="first"' })
+
+  const fourthPageScope = nock('https://api.github.com')
+    .get(`/repositories/27193779/pulls/${pr}/commits`)
+    .query({ page: 4 })
+    .reply(200, commitsFixturePage4, { link: '<https://api.github.com/repositories/27193779/pulls/9745/commits?page=3>; rel="prev", <https://api.github.com/repositories/27193779/pulls/9745/commits?page=1>; rel="first"' })
 
   t.plan(1)
 
   const commit = await findLatestCommitInPr({ owner, repo, pr })
   t.equal(commit.sha, 'c1aa949064892dbe693750686c06f4ad5673e577')
   firstPageScope.done()
-  lastPageScope.done()
+  secondPageScope.done()
+  thirdPageScope.done()
+  fourthPageScope.done()
 })
